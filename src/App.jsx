c83a040e3a93b711841f0d25d80caccfb9880371
ashishkandpal070896 <ashishkandpal58@gmail.com>
2025-05-28 17:02:37 +0530

    Ashish

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+// import './App.css'
+
+import { ToastContainer } from 'react-toastify'
+import Home from './pages/home'
+
+
+export default function App() {
+  return (
+    <>
+    <div>  
+      <Home/>
+      <ToastContainer/>
+
+    </div>
+   
+    </>
+  
+  )
+
+}
